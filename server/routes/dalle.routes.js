19fd7671b4d01e05cce87f8817d85a68a94f6e26
Code4Odisha <sohan@sohanchampion.com>
2024-05-05 23:54:22 +0530

    changes

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import {Configuration, OpenAIApi} from 'openai';
+import { Configuration,OpenAIApi} from 'openai';
+
 
 dotenv.config();
 
-const router =express.Router();
+const router = express.Router();
 
-router.route('/').get((req,res) => {
+router.route('/').get((req, res) => {
     res.status(200).json({ message: "Hello from DALL.E ROUTES"})
 })
 
-export default router
-
+export default router;
