9e473d4e09a1b3a49fb54f23ba58cdd89e923fd6
Code4Odisha <sohan@sohanchampion.com>
2024-06-01 18:13:51 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
-import dalleRoutes from './routes/dalle.routes.js';
+// import dalleRoutes from './routes/dalle.routes.js';
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limig: "50mb"}))
 
-app.use('/api/v1/dalle', dalleRoutes);
+// app.use('/api/v1/dalle', dalleRoutes);
 
 
 app.get('/', (req,res)  => {
